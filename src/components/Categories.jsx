bcdad276641b3fc3e7e176310e44a6065d2a6d93
EugeneTsalko <tefworkmail@gmail.com>
2023-05-27 20:48:39 +0300

    refactor: li render with map

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 const Categories = () => {
   const [activeIndex, setActiveIndex] = React.useState(0);
 
+  const categories = ['Все', 'Мясные', 'Вегетарианские', 'Острые', 'Закрытые'];
+
   const onClickCategory = (i) => {
     setActiveIndex(i);
   };
@@ -10,24 +12,11 @@ const Categories = () => {
   return (
     <div className="categories">
       <ul>
-        <li onClick={() => onClickCategory(0)} className={activeIndex === 0 ? 'active' : ''}>
-          Все
-        </li>
-        <li onClick={() => onClickCategory(1)} className={activeIndex === 1 ? 'active' : ''}>
-          Мясные
-        </li>
-        <li onClick={() => onClickCategory(2)} className={activeIndex === 2 ? 'active' : ''}>
-          Вегетарианская
-        </li>
-        <li onClick={() => onClickCategory(3)} className={activeIndex === 3 ? 'active' : ''}>
-          Гриль
-        </li>
-        <li onClick={() => onClickCategory(4)} className={activeIndex === 4 ? 'active' : ''}>
-          Острые
-        </li>
-        <li onClick={() => onClickCategory(5)} className={activeIndex === 5 ? 'active' : ''}>
-          Закрытые
-        </li>
+        {categories.map((category, i) => (
+          <li onClick={() => onClickCategory(i)} className={activeIndex === i ? 'active' : ''}>
+            {category}
+          </li>
+        ))}
       </ul>
     </div>
   );
