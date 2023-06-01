e8740bc2db3bee03e231f7ae5120f46a0c74b6b2
EugeneTsalko <tefworkmail@gmail.com>
2023-06-01 17:03:15 +0300

    feat: add routing to home and notfound

---
+++ src/pages/Home.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import Categories from '../components/Categories';
+import Sort from '../components/Sort';
+import PizzaBlock from '../components/PizzaBlock';
+import Skeleton from '../components/PizzaBlock/Skeleton';
+
+const Home = () => {
+  const [items, setItems] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    fetch('https://64760f5ce607ba4797dd3c3e.mockapi.io/items')
+      .then((res) => res.json())
+      .then((arr) => {
+        setItems(arr);
+        setIsLoading(false);
+      });
+  }, []);
+
+  return (
+    <>
+      <div className="content__top">
+        <Categories />
+        <Sort />
+      </div>
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">
+        {isLoading
+          ? [...new Array(6)].map((_, i) => <Skeleton key={i} />)
+          : items.map((pizza) => <PizzaBlock {...pizza} key={pizza.id} />)}
+      </div>
+    </>
+  );
+};
+
+export default Home;
