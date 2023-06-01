26f7b7e289ed528254539d693773590ae4d8898a
EugeneTsalko <tefworkmail@gmail.com>
2023-06-01 18:01:06 +0300

    feat: add cart route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
+
 import './scss/app.scss';
 import Header from './components/Header';
 import Home from './pages/Home';
+import Cart from './pages/Cart';
 import NotFound from './pages/NotFound';
 import { Route, Routes } from 'react-router-dom';
 
@@ -13,6 +15,7 @@ function App() {
         <div className="container">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </div>
